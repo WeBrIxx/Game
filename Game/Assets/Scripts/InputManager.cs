a78db74127677a27387fac8d59d3c2b9a23b7fad
WeBrIxx <tomik.borek@gmail.com>
2023-01-07 21:31:08 +0100

    Pes

---
+++ Game/Assets/Scripts/InputManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerInput PlayerInput; 
+    private PlayerInput.OnFootActions OnFoot;
+    
+    private PlayerMotor motor;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        PlayerInput = new PlayerInput();
+        Onfoot = PlayerInput.OnFoot;
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //tell the playermotor to move using the value from our movement action.
+        motor.ProccesmMove(OnFoot.Movement.ReadValue<Vector2>());
+    }
+    private void OnEnable()
+    {
+        OnFoot.Enable();
+    }
+
+    private void OnDisable()
+    {
+        OnFoot.Disable();
+    }
+}
