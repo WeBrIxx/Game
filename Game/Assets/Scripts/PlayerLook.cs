8fec47510b11a11dc35c0b73abe58b7a0c41fc42
SwiftikEU <cross11out112@seznam.cz>
2023-01-08 00:20:06 +0100

    Movement Done

--- Game/Assets/Scripts/PlayerLook.cs
+++ Game/Assets/Scripts/PlayerLook.cs
@@ -20,7 +20,7 @@ public class PlayerLook : MonoBehaviour
 
         cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
 
-        transform.Rotate(vector3.up * (mouseX *Time.deltaTime) * xSensitivity);
+        transform.Rotate(Vector3.up * (mouseX *Time.deltaTime) * xSensitivity);
 
 
     }
